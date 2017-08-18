c240e414d21657c0f5ac0eff7ace75fb843cca15
Manisha <mgmanishagahlot@gmail.com>
2017-08-18 10:19:20 +0100

    First Commit

---
+++ src/test/java/trade/model/InstructionTest.java
@@ -0,0 +1,29 @@
+package trade.model;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Currency;
+
+import org.junit.Test;
+
+import trade.model.Instruction.TradeAction;
+
+public class InstructionTest {
+
+	@Test
+	public void testTradeAmountCalculation() throws Exception {
+		final BigDecimal agreedFx = BigDecimal.valueOf(0.50);
+		final BigDecimal pricePerUnit = BigDecimal.valueOf(100.25);
+		final int units = 200;
+
+		final Instruction instruction = new Instruction("Entity1", TradeAction.BUY, LocalDate.of(2017, 8, 17),
+				LocalDate.of(2017, 8, 21), Currency.getInstance("SGD"), agreedFx, units, pricePerUnit);
+
+		final BigDecimal calculatedAmount = pricePerUnit.multiply(agreedFx).multiply(BigDecimal.valueOf(units)).setScale(3,
+				BigDecimal.ROUND_HALF_EVEN);
+		assertEquals(calculatedAmount, instruction.getTradeAmount());
+	}
+
+}
