ee5483301071591c13aa3c485aa9b7297137bf1f
Manisha <mgmanishagahlot@gmail.com>
2017-08-18 10:33:54 +0100

    First Commit

--- src/test/java/trade/model/InstructionTest.java
+++ src/test/java/trade/model/InstructionTest.java
@@ -18,8 +18,8 @@ public class InstructionTest {
 		final BigDecimal pricePerUnit = BigDecimal.valueOf(100.25);
 		final int units = 200;
 
-		final Instruction instruction = new Instruction("Entity1", TradeAction.BUY, LocalDate.of(2017, 8, 17),
-				LocalDate.of(2017, 8, 21), Currency.getInstance("SGD"), agreedFx, units, pricePerUnit);
+		final Instruction instruction = new Instruction("Entity1", TradeAction.SELL, LocalDate.of(2017, 8, 17),
+				LocalDate.of(2017, 8, 21), Currency.getInstance("GBP"), agreedFx, units, pricePerUnit);
 
 		final BigDecimal calculatedAmount = pricePerUnit.multiply(agreedFx).multiply(BigDecimal.valueOf(units)).setScale(3,
 				BigDecimal.ROUND_HALF_EVEN);
